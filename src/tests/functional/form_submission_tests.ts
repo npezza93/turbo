d4e9d4c25be8e1d815edb9d5ea0a6619b90670d3
Sean Doyle <sean.p.doyle24@gmail.com>
2021-01-03 11:25:54 -0500

    Add test coverage for turbo-frame[disabled]
    Support for the `[disabled]` attribute pre-dates this commit, but is
    largely untested. Prior to [hotwired/turbo-site#32][] and
    [hotwired/turbo-site#36][], there were no mentions of the attribute in
    the Turbo [Handbook][] or [Reference][] documentation.
    
    This commit adds functional test-level coverage for the existing
    behavior. When an `<a>` or `<form>` element targets a `<turbo-frame
    disabled>` element, Turbo skips intercepting and intervening, and
    instead navigates the page.
    
    Testing
    ---
    
    In addition, to avoid conflating `[data-turbo="false"]` and
    `<turbo-frame disabled>`, this commit also renames test and test fixture
    mentions of "disabled" to more accurately reflect which manner of
    skipping behavior is being tested.
    
    [Handbook]: https://github.com/hotwired/turbo-site/blob/52d5e7f1dfc26ac7a6e01c6a715857ccf5ed5081/docs/handbook/03_frames.md
    [Reference]: https://github.com/hotwired/turbo-site/blob/52d5e7f1dfc26ac7a6e01c6a715857ccf5ed5081/docs/reference/frames.md
    [hotwired/turbo-site#32]: https://github.com/hotwired/turbo-site/pull/32
    [hotwired/turbo-site#36]: https://github.com/hotwired/turbo-site/pull/36

--- src/tests/functional/form_submission_tests.ts
+++ src/tests/functional/form_submission_tests.ts
@@ -135,16 +135,16 @@ export class FormSubmissionTests extends TurboDriveTestCase {
 
   async "test form submission with Turbo disabled on the form"() {
     this.listenForFormSubmissions()
-    await this.clickSelector('#disabled form[data-turbo="false"] input[type=submit]')
+    await this.clickSelector('#turbo-false form[data-turbo="false"] input[type=submit]')
     await this.nextBody
     await this.querySelector("#element-id")
 
     this.assert.notOk(await this.turboFormSubmitted)
   }
 
-  async "test form submission with Turbo disabled on the submitter"() {
+  async "test form submission with [data-turbo=false] on the submitter"() {
     this.listenForFormSubmissions()
-    await this.clickSelector('#disabled form:not([data-turbo]) input[data-turbo="false"]')
+    await this.clickSelector('#turbo-false form:not([data-turbo]) input[data-turbo="false"]')
     await this.nextBody
     await this.querySelector("#element-id")
 
@@ -165,6 +165,14 @@ export class FormSubmissionTests extends TurboDriveTestCase {
     await this.nextBeat
 
     this.assert.notOk(await this.turboFormSubmitted)
+  }
+
+  async "test form submission targets disabled frame"() {
+    this.remote.execute(() => document.getElementById("frame")?.setAttribute("disabled", ""))
+    await this.clickSelector('#targets-frame [type="submit"')
+    await this.nextBody
+
+    this.assert.equal(await this.pathname, "/src/tests/fixtures/one.html")
   }
 
   listenForFormSubmissions() {
